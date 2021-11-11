a2ae27126aee35bbd123c812f6f895eb5dc38aad
NicolasNuttall <55877928+NicolasNuttall@users.noreply.github.com>
2021-11-11 11:01:31 +0000

    First commit

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,48 @@
-$(document).ready(function(){
+const phrases = ["Reading.","Notes.","Ideas."];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let wordEnded = false;
+let isEnd = false;
+
+function charLoop (){
+    isEnd = false;
     
+    if(i < phrases.length){
+      
+        if(!wordEnded && j <= phrases[i].length){
+            currentPhrase.push(phrases[i][j]);           
+            j++;
+            $("#loginSpan").text(currentPhrase.join(''));
+        }
+
+        if(wordEnded && j <= phrases[i].length){
+            currentPhrase.pop();
+            j--;
+            $("#loginSpan").text(currentPhrase.join(''));
+        }
+
+        if(j == phrases[i].length){
+            isEnd = true;
+            wordEnded = true;
+        }
+
+        if(wordEnded && j === 0){
+            currentPhrase = [];
+            wordEnded = false;
+            i++;
+            if(i === phrases.length){
+                i=0;
+            }
+        }
+        
+    }
+    const spedUp = Math.random() * (80 -50) + 50;
+    const normalSpeed = Math.random() * (200 -100) + 100;
+    const time = isEnd ? 2000 : wordEnded ? spedUp : normalSpeed;
+    setTimeout(charLoop, time);
+}
+
+$(document).ready(function(){
+    charLoop();
 })
