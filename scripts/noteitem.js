31fbdd45d4eef5a218baed3d7057c213651ec325
NicolasNuttall <55877928+NicolasNuttall@users.noreply.github.com>
2021-12-12 19:10:22 +0000

    Book Pages

--- scripts/noteitem.js
+++ scripts/noteitem.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $(".note-menu-icon").click(function(){
         $(".note-menu").toggle();
     }); 
+    $(".timer-menu").click(function(){
+        $(".timer-changer").toggleClass("revealed-menu");
+        $(".dropdown-arrow").toggleClass("upwardsarrow");
+    }); 
 
     $("body").on("click", ".loadmore", function(e){
         x = $(this).prev();
@@ -13,5 +17,13 @@ window.addEventListener('mouseup', function(e) {
     var x = document.querySelector('.note-menu');
     if (event.target != document.querySelector(".note-menu-icon")) {
         x.style.display = "none";
+    }
+});
+
+//
+window.addEventListener('mouseup', function(e) {
+    var x = $('timer-changer');
+    if (event.target != document.querySelector(".timer-menu")) {
+        x.toggleClass("revealed-menu");
     }
 });
